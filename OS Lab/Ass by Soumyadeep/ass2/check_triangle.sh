77c3f3be238cdad587d4695f034ac2e93ebefafc
THE-SPARTANS10 <jamesshadow0002@gmail.com>
2022-01-09 19:53:12 +0530

    All Done Yahhhhhhhhhhhhhhhhhhhhh

--- OS Lab/Ass by Soumyadeep/ass2/check_triangle.sh
+++ OS Lab/Ass by Soumyadeep/ass2/check_triangle.sh
@@ -6,15 +6,14 @@ read b
 echo -n "Side3: "
 read c
 
-s=`echo "($a + $b + $c) /2"|bc`
-echo $s
-x=`echo "$s * ($s - $a)*($s -$b)*($s-$c)"|bc`
+s=`echo "($a + $b + $c) /2"|bc -l`
+x=`echo "$s * ($s - $a)*($s -$b)*($s-$c)"|bc -l`
 x1=`echo "$x>0"|bc`
 
 if [ $x1 -gt 0 ]
 then
 	echo "Triangle formation is possible"
-	ar=`echo "scale=0; sqrt($x)"|bc`
+	ar=`echo "scale=0; sqrt($x)"|bc -l`
 	echo "Area is $ar"
 else
 	echo "Triangle formation is not possible"
