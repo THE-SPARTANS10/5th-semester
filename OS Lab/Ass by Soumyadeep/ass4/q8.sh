228771269542b74c97d7684641d5fd8f9a54c249
THE-SPARTANS10 <jamesshadow0002@gmail.com>
2022-01-09 14:41:18 +0530

    uploaded by soumyadeep

--- OS Lab/Ass by Soumyadeep/ass4/q8.sh
+++ OS Lab/Ass by Soumyadeep/ass4/q8.sh
@@ -1,9 +1,27 @@
 clear
-TODAY=$(date +"%Y-%m-%d")
-echo "May I know your name"
+echo -n "May I know your name: "
 read name
-echo "Your birthdate(yyyy-mm-dd)"
-read birthdate
-tmpDays=$( printf '%s' $(( $(date -u -d"$TODAY" +%s) - $(date -u -d"$birthdate" +%s)))  )
-age=$(( $tmpDays / 60 / 60 / 24 / 364 ))
-echo Hey $name you are $age years old.
+echo "Date of Birth(dd/mm/yyyy)"
+read dob
+IFS="/"  #Seperator
+set $dob  #Arranges in Conventional Form
+d=$1
+m=$2
+y=$3
+echo "Test $d $m $y"
+d1=`date +%d`
+m1=`date +%m`
+y1=`date +%Y` #Y represents 4-digit , y represents 2-digit
+echo "Today is $d1/$m1/$y1"
+dd=`expr $d1 - $d`  #Difference in Present and Birth date
+mm=`expr $m1 - $m`  #Difference in Present and Birth month
+yy=`expr $y1 - $y`  #Difference in Present and Birth year
+if [ $dd -lt 0 ]; then  #Date Negative
+  mm=`expr $mm - 1`
+  dd=`expr $dd + 30`  #Adjusted Date
+  if [ $mm -lt 0 ]; then
+    yy=`expr $yy - 1`
+    mm=`expr $mm + 12` #Adjusted month
+  fi
+fi
+echo "Hey $name your age is $yy year, $mm months , $dd days "
