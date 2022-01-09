860c2b8bc44d61098b721b6b775d2036f519d154
THE-SPARTANS10 <jamesshadow0002@gmail.com>
2022-01-09 06:46:19 +0530

    uploaded by soumyadeep

--- OS Lab/Ass by Soumyadeep/ass4/q12.sh
+++ OS Lab/Ass by Soumyadeep/ass4/q12.sh
@@ -1,1 +1,27 @@
-date -d "+%D"
+clear
+rm Weather.txt
+file1=input_weather.txt
+echo "DAY-|-TEMP-|-WTR" >> Weather.txt
+echo "~~~~|~~~~~~|~~~~" >> Weather.txt
+if [ -f $file1 ]
+then
+  exec<$file1
+  while read line
+  do
+    set `echo $line`
+    echo -n $1 "| " $2 " | " >> Weather.txt
+    if [ $2 -lt 15 ]
+    then 
+      echo Very Cold >> Weather.txt
+    elif [ $2 -lt 25 ]
+    then 
+      echo Cold >> Weather.txt
+    elif [ $2 -lt 35 ]
+    then 
+      echo Hot >> Weather.txt
+    else 
+      echo Very Hot >> Weather.txt
+    fi
+  done
+fi
+cat Weather.txt
