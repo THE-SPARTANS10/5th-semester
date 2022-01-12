310207ffe27777b0bb6335e748231e885b35a90f
subho2107 <shubho2001g@gmail.com>
2022-01-12 13:11:50 +0530

    changes made

--- OOPs Assignment/Assignment 9(Exception Handling)/Six.java
+++ OOPs Assignment/Assignment 9(Exception Handling)/Six.java
@@ -1,20 +1,24 @@
 import java.io.*;
 class PayOutOfBoundException extends Exception{
-    PayOutOfBoundException(String errorMessage){
-        super(errorMessage);
-    }
-    PayOutOfBoundException(){
-        super("Sorry company cant pay you so much money!!");
+    @Override
+    public String toString() {
+        return "Sorry company cant pay you so much money!!";
     }
 }
 public class Six {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter your salary");
         int salary = Integer.parseInt(buffer.readLine());
         int maxSalary = 9999;
-        if (salary > maxSalary)
-            throw new PayOutOfBoundException();
-        else
+        try
+        {
+            if (salary > maxSalary)
+                throw new PayOutOfBoundException();
             System.out.println("Okay company can pay you this much");
+        }
+        catch (PayOutOfBoundException pe) {
+            System.out.println(pe);
+        }
     }
 }
